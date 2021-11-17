5a7fc024c98c0c19437f4c9b6eb7af5c7f37d818
CesarAndresTICS <94024917+CesarAndresTICS@users.noreply.github.com>
2021-11-16 23:08:47 -0600

    Cesar
    Contacto
    Configuracion de botones

--- app/src/main/java/mx/ita/securityhome/Contacto.java
+++ app/src/main/java/mx/ita/securityhome/Contacto.java
@@ -43,7 +43,10 @@ public class Contacto extends AppCompatActivity {
         gmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "securityhome21@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Contact");
+                intent.putExtra(Intent.EXTRA_TEXT, "♥");
+                startActivity(intent);
             }
         });
 
