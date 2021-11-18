1cfb681a1f12831c68f72ccdbefbf1326ab03908
RubiResendiz <85216340+RubiResendiz@users.noreply.github.com>
2021-11-17 19:27:15 -0600

    Revert "Security Home test"
    This reverts commit 0d2e725e54284b63c510b62d7d69702a889ea91b.

--- app/src/main/java/mx/ita/securityhome/Reportebug.java
+++ app/src/main/java/mx/ita/securityhome/Reportebug.java
@@ -2,13 +2,47 @@ package mx.ita.securityhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Reportebug extends AppCompatActivity {
+
+    TextView para, asunto, mensaje;
+    Button enviar;
+    String sPara, sAsunto, sMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportebug);
+
+        para = (EditText) findViewById(R.id.ePara);
+        asunto = (EditText) findViewById(R.id.eAsunto);
+        mensaje = (EditText) findViewById(R.id.eMensaje);
+        para.setText("findmybusiness@gmail.com");
+        enviar = (Button) findViewById(R.id.btnEnviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sPara = para.getText().toString();
+                sAsunto = asunto.getText().toString();
+                sMensaje = mensaje.getText().toString();
+                sPara = "findmybusiness@gmail.com";
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ sPara});
+                email.putExtra(Intent.EXTRA_SUBJECT, sAsunto);
+                email.putExtra(Intent.EXTRA_TEXT, sMensaje);
+
+                //necesita esto para solicitar solo al cliente de correo electrónico
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Elija el cliente de correo electrónico :"));
+            }
+        });
     }
 }
