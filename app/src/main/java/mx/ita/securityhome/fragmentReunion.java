d9ad69f9a4864cb2a3bf780a0fce2e7699dd498f
MemoBernalTec <78555373+Carlosp1@users.noreply.github.com>
2021-11-26 20:57:07 -0600

    Guillermo Bernal
    uptade

--- app/src/main/java/mx/ita/securityhome/fragmentReunion.java
+++ app/src/main/java/mx/ita/securityhome/fragmentReunion.java
@@ -3,10 +3,25 @@ package mx.ita.securityhome;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class fragmentReunion extends Fragment {
+    List<String> reuniones;
+    List<ListElementReuniones> elements;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    String userID = mAuth.getCurrentUser().getUid();
+    String dato;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +80,69 @@ public class fragmentReunion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reunion, container, false);
+      View root = inflater.inflate(R.layout.fragment_reunion, container, false);
+      init(root);
+      return root;
+    }
+    public void init(View root){
+        reuniones = new ArrayList<>();
+        Query query = ref.child("reuniones").orderByChild("id_reunion");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot citi : dataSnapshot.getChildren()){
+                        String id_reunion = citi.child("id_reunion").getValue().toString();
+                        String id_residente = citi.child("id_residente").getValue().toString();
+                        Log.i("String PRUEBA", id_residente+"");
+                        if(id_residente.equals(userID))
+                            reuniones.add(id_reunion);
+                    }
+                }
+                Log.i("número de reuniones", reuniones.size()+"");
+                Log.i("INIT","OK_________________-");
+                elements = new ArrayList<>();
+
+                for(String reunion : reuniones){
+                    Query AXD = ref.child("reuniones").orderByChild("id_reuniones");
+                    Log.i("Valores reuniones","No me quieren!");
+                    AXD.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot citi : dataSnapshot.getChildren()) {
+                                    String id_reunion = citi.child("id_reunion").getValue().toString();
+                                    Log.i("Imprimirrrrr", id_reunion);
+                                    if(id_reunion.equals(reunion)){
+                                        Log.i("jirijirijiri", "Sí entró");
+                                        String fechainicio = citi.child("fechainicio").getValue(String.class);
+                                        String horainicio = citi.child("horainicio").getValue(String.class);
+                                        Log.i("Valores reunion",id_reunion + " " + fechainicio);
+                                        elements.add(new ListElementReuniones(fechainicio, horainicio, id_reunion));
+                                        Log.i("número dereuniones :D", reuniones.size()+"");
+                                        ListAdapterReuniones listAdapter = new ListAdapterReuniones(elements, getContext());
+                                        RecyclerView recyclerView = root.findViewById(R.id.recicladoraReunion);
+                                        recyclerView.setHasFixedSize(true);
+                                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                                        recyclerView.setAdapter(listAdapter);
+
+                                    }
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Toast.makeText(getContext(),"Fallo la lectura: " + databaseError.getCode(),Toast.LENGTH_LONG);
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getContext(),"Fallo la lectura: " + databaseError.getCode(),Toast.LENGTH_LONG);
+            }
+        });
+
     }
 }
