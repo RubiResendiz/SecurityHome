8b557e3234a405d010b301a5aaa6ea34aa125a72
MemoBernalTec <78555373+Carlosp1@users.noreply.github.com>
2021-11-28 14:34:37 -0600

    Guillermo
    Update

--- app/src/main/java/mx/ita/securityhome/editarReunion.java
+++ app/src/main/java/mx/ita/securityhome/editarReunion.java
@@ -33,5 +33,169 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class editarReunion extends AppCompatActivity {
+    ImageButton regresar;
+    private FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+    Button confirm;
+    String id, id_residente;
+    final Calendar calendario = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_reunion);
+        TextView txtFecha1 = findViewById(R.id.pickDate1E);
+        TextView txtFecha2 = findViewById(R.id.pickDate2E);
+        TextView txtHora1 = findViewById(R.id.pickHour1E);
+        TextView txtHora2 = findViewById(R.id.pickHour2E);
 
+        txtFecha1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseDate(txtFecha1);
+            }
+        });
+        txtFecha2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseDate(txtFecha2);
+            }
+        });
+        txtHora1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseTime(txtHora1);
+            }
+        });
+        txtHora2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseTime(txtHora2);
+            }
+        });
+
+
+        Bundle bundle = getIntent().getExtras();
+        String dato=bundle.getString("id_reunion");
+        Log.i("Dato recibido", dato);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        Button confirmar = findViewById(R.id.btnGuardarEditReunion);
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id_residente", id_residente);
+                map.put("id_reunion", dato);
+                map.put("fechainicio", txtFecha1.getText().toString());
+                map.put("fechafin", txtFecha2.getText().toString());
+                map.put("horainicio", txtHora1.getText().toString());
+                map.put("horafin", txtHora2.getText().toString());
+
+
+
+                mDatabase.child("reuniones").child(dato).
+                        setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task2) {
+                        if(task2.isSuccessful()){
+                            Log.i("Todo bien", "Todo bien master");
+                            startActivity(new Intent(editarReunion.this, invitadoslista.class));
+                        }else{
+                            Log.w("Error XD 1",task2.getException());
+                        }
+                    }
+                });
+
+            }
+        });
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        regresar = findViewById(R.id.btnReturnListaEditReu);
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(editarReunion.this, invitadoslista.class);
+                startActivity(intent);
+            }
+        });
+        ref.child("reuniones").child(dato).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String fechainicio = dataSnapshot.child("fechainicio").getValue(String.class);
+                    String fechafin = dataSnapshot.child("fechafin").getValue(String.class);
+                    String horainicio = dataSnapshot.child("horainicio").getValue(String.class);
+                    String horafin = dataSnapshot.child("horafin").getValue(String.class);
+                    id_residente = dataSnapshot.child("id_residente").getValue().toString();
+
+                    txtFecha1.setText(fechainicio);
+                    txtFecha2.setText(fechafin);
+                    txtHora1.setText(horainicio);
+                    txtHora2.setText(horafin);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(editarReunion.this,"Fallo la lectura: " + databaseError.getCode(),Toast.LENGTH_LONG);
+            }
+        });
+    }
+    private void chooseTime(TextView date_field) {
+
+        final Calendar calendar = Calendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                calendario.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendario.set(Calendar.MINUTE, minute);
+
+                String dateString = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+                date_field.setText(dateString); // set the date
+            }
+        },hour,minute,true);
+
+        timePicker.show();
+
+        timePicker.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(final DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void chooseDate(TextView date_field) {
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePicker =
+                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(final DatePicker view, final int year, final int month,
+                                          final int dayOfMonth) {
+
+                        @SuppressLint("SimpleDateFormat")
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        calendar.set(year, month, dayOfMonth);
+                        String dateString = sdf.format(calendar.getTime());
+
+                        date_field.setText(dateString); // set the date
+                    }
+                }, year, month, day); // set date picker to current date
+
+        datePicker.show();
+
+        datePicker.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(final DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
+    }
 }
--- app/src/main/java/mx/ita/securityhome/registroTemporal.java
+++ app/src/main/java/mx/ita/securityhome/registroTemporal.java
@@ -21,4 +21,42 @@ import mx.ita.securityhome.ui.login.LoginActivity;
 
 public class registroTemporal extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_temporal);
+        mAuth = FirebaseAuth.getInstance();
+        TextView correo = findViewById(R.id.txtCorreoTemporal);
+        TextView contrasena = findViewById(R.id.txtContrasTemporal);
+        Button btnregistro = findViewById(R.id.btnRegistroTemp);
+        btnregistro.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String correos = correo.getText().toString();
+                String contrasenas = contrasena.getText().toString();
+                mAuth.createUserWithEmailAndPassword(correos, contrasenas)
+                        .addOnCompleteListener(registroTemporal.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("Message", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent myIntent = new Intent(registroTemporal.this, LoginActivity.class);
+                                    registroTemporal.this.startActivity(myIntent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Message", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(registroTemporal.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+
+        });
+    }
 }
