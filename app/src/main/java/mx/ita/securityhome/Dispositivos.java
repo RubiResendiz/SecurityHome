1dc23afcb2ecdace6f2255ce9cf8ff3c7c94df51
RubiResendiz <85216340+RubiResendiz@users.noreply.github.com>
2021-11-29 22:47:03 -0600

    Revert "Security Home test"
    This reverts commit 0d2e725e54284b63c510b62d7d69702a889ea91b.

--- app/src/main/java/mx/ita/securityhome/Dispositivos.java
+++ app/src/main/java/mx/ita/securityhome/Dispositivos.java
@@ -1,20 +1,55 @@
 package mx.ita.securityhome;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 
 public class Dispositivos extends AppCompatActivity {
-    /**
- *codigo java de los dispositivos a conectar 
- * 
- * 
- */
+    static private String IP_HOST = "192.168.100.202"; //posible cambio para meter la IP manualmente
+    static private int PORT = 9999;
+    private byte[] buf;
+    Bitmap bp;
+    String buff;
+    private DatagramSocket reveSocket;
+
+    private ProgressDialog pDialog;
+    static private ImageView imgShow; //aquí vamos a mostrar el video
+    static private TextView estado; //si está online u opffline
+    static private EditText capturar_ip;
+    static private Button conectar;
+    Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +57,25 @@ public class Dispositivos extends AppCompatActivity {
         setContentView(R.layout.activity_dispositivos);
         ImageButton settings = findViewById(R.id.btnSettings);
         ImageButton back = findViewById(R.id.btnBackDevice);
+        imgShow = findViewById(R.id.salidaVideo);
+        estado = findViewById(R.id.txtEstado);
+        capturar_ip = findViewById(R.id.txtIPHost);
+        conectar = findViewById(R.id.btnConectar);
+        conectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!capturar_ip.getText().toString().equals("")){
+                    try{
+                        new actualizar().execute(); //tarea asíncrona
+                    }catch (Exception e){
+                        Toast.makeText(Dispositivos.this,"OFFLINE", Toast.LENGTH_SHORT);
+                    }
+
+                }
+            }
+        });
+
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,4 +89,65 @@ public class Dispositivos extends AppCompatActivity {
             }
         });
     }
+
+
+    public class actualizar extends AsyncTask<String, Bitmap, String> {
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected String doInBackground(String... params) {
+            IP_HOST = capturar_ip.getText().toString();
+            Log.i("ola","corriendo");
+            try {
+                boolean XD = true;
+                socket = new Socket(IP_HOST,PORT); //creamos socket con los datos del server
+                int conteo=0;
+                BufferedReader mBufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                //buffer para contener los packetes con 3 secciones
+
+                while(socket.isConnected()){
+                    String xxx = mBufferIn.readLine(); //encabezado del formato utf-8
+                    Log.i("CONTEO: " , (conteo++)+"-----------------");
+                    int size = Integer.parseInt(xxx);
+                    String next = mBufferIn.readLine(); //segunda línea es la imagen en base64 (string)
+                    InputStream targetStream = new ByteArrayInputStream(next.getBytes()); //guardar base 64 en InputStream
+                    Log.i("Printing image",next.length()+" ::: " + size + " ::: " + targetStream.available());
+
+                    Log.i("substring", next.substring(size,next.length()));
+
+                   /* int maxLogSize = 1000;
+                    for(int i = 0; i <= next.length() / maxLogSize; i++) {
+                        int start = i * maxLogSize;
+                        int end = (i+1) * maxLogSize;
+                        end = end > next.length() ? next.length() : end;
+                        Log.v("OOOOOO2", next.substring(start, end));
+                    }*/
+
+                    byte[] decodedString = Base64.decode(next, Base64.DEFAULT); //decodificamos base64
+                    //
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    this.publishProgress(decodedByte); //publicar la imagen porque es una tarea asíncrona
+                    String facherito = mBufferIn.readLine(); //separador entre paquetes
+                }
+
+            } catch (Exception e) {
+                estado.setText("OFFLINE");
+                estado.setTextColor(Color.RED);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Bitmap... values) {
+            super.onProgressUpdate(values);
+            imgShow.setImageBitmap(values[0]);
+            estado.setTextColor(Color.GREEN);
+            estado.setText("ONLINE");
+            conectar.setEnabled(false);
+        }
+
+    }
+    private static int MAX_CONNECTION = 10;
+    private int reconnections = 0;
 }
--- app/src/main/java/mx/ita/securityhome/agregarReunion.java
+++ app/src/main/java/mx/ita/securityhome/agregarReunion.java
@@ -1,33 +1,86 @@
 package mx.ita.securityhome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 
 public class agregarReunion extends AppCompatActivity {
-    TextView pickdate1, pickdate2;
+    TextView pickdate1, pickdate2, pickhour1, pickhour2;
+    Button btn;
+    private FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
     final Calendar calendario = Calendar.getInstance();
-    int anio = calendario.get(Calendar.YEAR);
-    int mes = calendario.get(Calendar.MONTH);
-    int diaDelMes = calendario.get(Calendar.DAY_OF_MONTH);
-    private int ultimoAnio, ultimoMes, ultimoDiaDelMes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_reunion);
         ImageButton btnReturn = findViewById(R.id.btnReturnLista);
+
         pickdate1 = findViewById(R.id.pickDate1);
         pickdate2 = findViewById(R.id.pickDate2);
-        refrescarFechaEnEditText();
+        pickhour1 = findViewById(R.id.pickHour1);
+        pickhour2 = findViewById(R.id.pickHour2);
+        btn = findViewById(R.id.btnGuardarReunion);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth = FirebaseAuth.getInstance();
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+                //objeto para guardar valores que serán subidos a firebase
+                Map<String, Object> map = new HashMap<>();
+                String IDnuevo = mAuth.getCurrentUser().getUid();
+                //mapear información para insertar en firebase
+                //sql = insert into nombre, genero, re
+                String id_reunion = agregarResidente.generateRandomPassword(15);
+                map.put("id_reunion",id_reunion);
+                map.put("fechainicio", pickdate1.getText().toString());
+                map.put("fechafin", pickdate2.getText().toString());
+                map.put("horainicio", pickhour1.getText().toString());
+                map.put("horafin",pickhour2.getText().toString());
+                map.put("id_residente", IDnuevo);
+                //nos vamos a la tabla reuniones e insertamos el mapa de datos anterior
+                mDatabase.child("reuniones").child(id_reunion).
+                        setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task2) {
+                        if(task2.isSuccessful()){
+                            Log.i("Todo bien", "Todo bien master");
+                            startActivity(new Intent(agregarReunion.this, invitadoslista.class));
+                        }else{
+                            Log.w("Error XD 1",task2.getException());
+                        }
+                    }
+                });
+
+            }
+        });
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,27 +88,88 @@ public class agregarReunion extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        pickhour1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseTime(pickhour1);
+            }
+        });
+        pickhour2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseTime(pickhour2);
+            }
+        });
         pickdate1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog dialogoFecha = new DatePickerDialog(agregarReunion.this, listenerDeDatePicker, ultimoAnio, ultimoMes, ultimoDiaDelMes);
-                dialogoFecha.show();
+                chooseDate(pickdate1);
+            }
+        });
+        pickdate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseDate(pickdate2);
+            }
+        });
+
+    }
+    //programación del calendario
+    private void chooseTime(TextView date_field) {
+
+        final Calendar calendar = Calendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                calendario.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendario.set(Calendar.MINUTE, minute);
+
+                String dateString = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+                date_field.setText(dateString); // set the date
+            }
+        },hour,minute,true);
+
+        timePicker.show();
+
+        timePicker.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(final DialogInterface dialog) {
+                dialog.dismiss();
             }
         });
     }
-    public void refrescarFechaEnEditText() {
-        // Formateamos la fecha pero podríamos hacer cualquier otra cosa ;)
-        String fecha = String.format(Locale.getDefault(), "%02d-%02d-%02d", ultimoAnio, ultimoMes+1, ultimoDiaDelMes);
 
-        // La ponemos en el editText
-        pickdate1.setText(fecha);
+    private void chooseDate(TextView date_field) {
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePicker =
+                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(final DatePicker view, final int year, final int month,
+                                          final int dayOfMonth) {
+
+                        @SuppressLint("SimpleDateFormat")
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        calendar.set(year, month, dayOfMonth);
+                        String dateString = sdf.format(calendar.getTime());
+
+                        date_field.setText(dateString); // set the date
+                    }
+                }, year, month, day); // set date picker to current date
+
+        datePicker.show();
+
+        datePicker.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(final DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
     }
-    // Crear un listener del datepicker;
-    private DatePickerDialog.OnDateSetListener listenerDeDatePicker = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int anio, int mes, int diaDelMes) {
-            // Esto se llama cuando seleccionan una fecha. Nos pasa la vista, pero más importante, nos pasa:
-            // El año, el mes y el día del mes. Es lo que necesitamos para saber la fecha completa
-        }
-    };
 }
